f64be2c1222e8728e163ae911d951cc08a2085ce
Tanisha-0874 <tanishachekkala@gmail.com>
2025-07-17 22:34:56 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int,int>hm;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){ 
             int y=target-nums[i];
             if(hm.find(y)!=hm.end()){
                 return {i,hm[y]};
